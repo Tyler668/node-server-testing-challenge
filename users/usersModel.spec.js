06e510d0c7b3a5a437728fcd0495d728371149d2
Tyler Gilchrist <tyler668@gmail.com>
2019-10-17 17:46:06 -0400

    Testing complete for delete, MVp done

--- users/usersModel.spec.js
+++ users/usersModel.spec.js
@@ -12,9 +12,10 @@ describe('Users Model', () => {
     });
 
 
-
-    describe('add()', () => {
-        it('Should add a user to the database', async () => {
+// vv ADD vv ------------------------------------------------------------------------------------------------------
+    
+describe('add()', () => {
+        it('Should add a user to the database when invoked', async () => {
 
             // Check that the table is empty
             const records = await db('users');
@@ -42,6 +43,60 @@ describe('Users Model', () => {
 
         const users = await db('users');
         expect(users).toHaveLength(2);
+
+
+
+    });
+   
+// vv REMOVE vv ------------------------------------------------------------------------------------------------------
+    
+    describe('remove()', () => {
+        it('Should remove a user from the database when invoked', async () => {
+            
+            //Insert one user
+            const bananaUser = await Users.add({ name: 'Banana' });
+            
+            // Check that the table has one record
+            const records = await db('users');
+            expect(records).toHaveLength(1);
+
+            //Remove one user
+            
+            await Users.remove(bananaUser.id);
+
+            //Check we now have zero records in the table after deleting
+            const users = await db('users');
+            expect(users).toHaveLength(0);
+
+
+        });
+    });
+
+    it('Should remove the specified user object from the user DB', async () =>{
+       
+        const addedUser = await Users.add({ name: 'Tyler' });
+        expect(addedUser.name).toBe('Tyler');
+
+        await Users.add({ name: 'Tony' });
+
+
+        
+        await Users.remove(addedUser.id);
+        let users = await db('users');
+        expect(users)
+        .not
+        .toContainEqual({"id": 1, "name": "Tyler"});  //No does not contain Tyler
+        
+        //Yes contains Tony
+        expect(users)
+        .toContainEqual({"id": 2, "name": "Tony"});
+        
+        
+
+        
+
+        
+        // expect(users).toHaveLength(1);
 
 
 
